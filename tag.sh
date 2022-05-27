cb897849728d9df00d26713b7d2938f2fb3a9ccf
ArunDevops123 <92020349+ArunDevops123@users.noreply.github.com>
2022-05-27 16:35:16 +0530

    tag-v2.0.sh

--- tag.sh
+++ tag.sh
@@ -1,9 +1,36 @@
-#!/bin/bash
+#!/bin/sh
+
+####################################
+######### Changing root user #######
+
 if [ "$(whoami)" != "root" ]
 then
     sudo su -s "$0"
     exit
 fi
+
+#############################################
+########### Condition Checking ##############
+node_count=$(cat count.txt | awk '{ print $3 }')
+a=0
+b=1
+until [ $a -ge $node_count ] && [ $b -eq 0 ]
+do
+    echo $a
+
+b=$(kubectl get no | awk '{ print $2 }' | grep "NotReady" | wc -l)
+    sleep 1
+
+count=$(kubectl get no | wc -l)
+a="$((count - 1))"
+echo "$a"
+
+done
+
+echo "Passed"
+
+####################################################
+################## Tag Attachment ##################
 
 variable='database microservice mnm npn'
 
@@ -18,7 +45,7 @@ do
             echo $i
             name=$(kubectl get no | awk '{ print $1 }' | grep $val | awk 'FNR=='$i' {print}')
             echo "$name"
-            kubectl label node $name group=$val --overwrite
+            kubectl label node $name group5=$val --overwrite
             i=$(($i+1))
         done
     else
