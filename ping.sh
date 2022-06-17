f20e51127c4de122d65011968ec136b02e1e7cca
ArunDevops123 <92020349+ArunDevops123@users.noreply.github.com>
2022-06-17 13:23:52 +0530

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-ipaddress=\`route -n | grep tunl0 | awk -F' ' '{print \$1, \$2}'\`
-ipaddress=\`echo \$ipaddress | tr \"\n\" \" \"\`
-iparray=(\$ipaddress);
-for ((index=0; index<\${#iparray[@]}; index=\${index}+2)); do
-  ping -c 3 \${iparray[\${index}]}
-done
+ipaddress=`route -n | grep tunl0 | awk -F' ' '{print $1, $2}'`
+ipaddress=`echo $ipaddress | tr "\n" " "`
+iparray=($ipaddress);
+for ((index=0; index<${#iparray[@]}; index=${index}+2)); do
+  ping -c 3 ${iparray[${index}]}
+done  
